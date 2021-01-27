a990d8e1c3b6ef4f3f88f50b96196d08c140f938
AntoineLibert <duncan-arrakis@hotmail.fr>
2021-01-27 19:14:49 +0100

    [Plant] LeafMotor : script for growing leaf progressivelly

---
+++ Assets/Plants/LeafMotor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafMotor : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float factor = 0.1f;
+        if (transform.localScale.x < 6)
+        {
+            transform.localScale += new Vector3(factor, factor, factor);
+        }
+    }
+}
