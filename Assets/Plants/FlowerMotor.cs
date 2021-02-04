7f14be4851414bc539716c55cf9ac807bb216fa7
AntoineLibert <duncan-arrakis@hotmail.fr>
2021-02-04 12:20:23 +0100

    [Plant] FlowerMotor : Add flower progressive scale, and randow finalsize

--- Assets/Plants/FlowerMotor.cs
+++ Assets/Plants/FlowerMotor.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 namespace PlantAI
 {
-    public class LeafMotor : MonoBehaviour
+    public class FlowerMotor : MonoBehaviour
     {
+
         public float finalScale = 5;
 
         // Start is called before the first frame update
         void Start()
         {
-            finalScale += Random.Range(-3, 1);
-            transform.localScale = new Vector3(1, 1, 1);
+            finalScale += Random.Range(-2f, 1f);
+            transform.localScale = new Vector3(0.1f, 0.1f, 0.05f);
         }
 
 
         // Update is called once per frame
         void Update()
         {
-            float factor = 0.1f;
+            float factor = 0.01f;
             if (transform.localScale.x < finalScale)
             {
-                transform.localScale += new Vector3(factor, factor, factor);
+                transform.localScale += new Vector3(factor, factor / 2f, factor);
             }
         }
     }
