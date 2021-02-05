2a8b2b92751a9feadcc67a167386685737e4a281
rubenbrami <45846602+rubenbrami@users.noreply.github.com>
2021-02-05 15:08:21 +0100

    ScriptsInteractivity

---
+++ Assets/Scripts/MouseOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOver : MonoBehaviour
+{
+    bool open = false;
+
+    void OnMouseDown()
+    {
+        print(transform.position.x);
+        
+            if (!open)
+            {
+                transform.position = new Vector3(transform.position.x+10, transform.position.y, transform.position.z);
+                open = true;
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x-10, transform.position.y, transform.position.z);
+                open = false;
+            }
+
+        
+        
+    }
+}
