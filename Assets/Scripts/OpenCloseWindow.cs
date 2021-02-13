2e98976946721c742179935455dd4eb271727c69
Mallet Flora <floramallet@outlook.fr>
2021-02-13 11:20:02 +0100

    [UI] Add a welcome panel with commands

---
+++ Assets/Scripts/OpenCloseWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseWindow : MonoBehaviour
+{
+    private Canvas welcomePanel;
+
+    void Start()
+    {
+        welcomePanel = GetComponent<Canvas>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            welcomePanel.enabled = !welcomePanel.enabled;
+        }
+    }
+}
