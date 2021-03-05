decfb2410151da7fa18692bd6c23b69d18905da6
Julien-Haudegond <44610840+Julien-Haudegond@users.noreply.github.com>
2021-03-05 12:51:11 +0100

    :pencil: LightFpsController with the mouse wheel

---
+++ Assets/Scripts/LightFpsController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightFpsController : MonoBehaviour
+{
+    float intensity = 0f;
+    float scale = 0.25f;
+    Light spotLight;
+
+    void Awake()
+    {
+        spotLight = GetComponent<Light>();
+        spotLight.intensity = intensity;
+    }
+
+    void Update()
+    {
+        // Compute the intensity value.
+        intensity += Input.mouseScrollDelta.y * scale;
+        intensity = Mathf.Clamp(intensity, 0f, 10f);
+
+        // Assign it to the spotlight.
+        spotLight.intensity = intensity;
+    }
+}
